aa928d77d7a38361e8b9806d0caa51f743dbc4b0
A Pixel a Day Keeps the Doctor Away <blaise+dummy@gmail.com>
2014-06-27 22:46:43 +0200

    Echo a few states in shell

--- commit.sh
+++ commit.sh
@@ -3,6 +3,10 @@ file=$2
 message=$3
 
 cd "$dir"
+
+echo "Committing..."
 git add "$file"
 git commit -m "$message" --author="blaisekal+dummy@gmail.com"
+
+echo "Pushing..."
 git push
--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@ repository=$2
 email=$3
 name=$4
 
+echo "Cloning..."
 git clone $repository $dir
 cd $dir
+
+echo "Setting git config..."
 git config user.email "$email"
 git config user.name "$name"
